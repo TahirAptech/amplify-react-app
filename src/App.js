a5c6341a86f38427f96fb812975427fcad618a95
mughees605 <muhammadmughees08@gmail.com>
2023-10-30 18:24:14 +0500

    first commit2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Salam, Tahir Here.
+          Salam, Tahir2.
         </p>
         <a
           className="App-link"
