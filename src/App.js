d0fa3482ae7c6bbf7c2c7e1576ca664e5c1ea65c
mughees605 <muhammadmughees08@gmail.com>
2023-10-30 18:26:25 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Salam, Tahir2.
+          Salam, I am Tahir.
         </p>
         <a
           className="App-link"
